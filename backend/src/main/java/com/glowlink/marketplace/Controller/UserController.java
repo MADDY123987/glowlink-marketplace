dc11713484b0eada99fb8265beab8c84e8bfe3c3
Madhavan <madhavan20041@gmail.com>
2026-06-20 22:50:51 +0530

    Exception Handler Added

--- backend/src/main/java/com/glowlink/marketplace/Controller/UserController.java
+++ backend/src/main/java/com/glowlink/marketplace/Controller/UserController.java
@@ -1,11 +1,14 @@
 package com.glowlink.marketplace.Controller;
 
 import com.glowlink.marketplace.Model.User;
+import com.glowlink.marketplace.Repository.UserRepository;
 import com.glowlink.marketplace.Service.UserService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/users")
@@ -13,10 +16,12 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+    @Autowired
+    private UserRepository userRepository;
 
     // POST Request
     @PostMapping
-    public User createUser(@RequestBody User user) {
+    public User createUser(@RequestBody @Valid User user) {
         return userService.createUser(user);
     }
 
@@ -24,5 +29,36 @@ public class UserController {
     @GetMapping
     public List<User> getAllUsers() {
         return userService.getAllUsers();
+    }
+    @GetMapping("/id/{userId}")
+    public User getUserById(@PathVariable Long userId) {
+        return userRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+    }
+    @GetMapping("/test/{userId}")
+    public String test(@PathVariable Long userId) {
+        return "ID = " + userId;
+    }
+    @PutMapping("/id/{id}")
+    public User updateUser(@RequestBody User user,
+                           @PathVariable Long id) throws Exception{
+        Optional<User> otp=userRepository.findById(id);
+        if(otp.isEmpty()){
+            throw new Exception("user not found with id"+id);
+        }
+        User existingUser=otp.get();
+        existingUser.setFullName(user.getFullName());
+        existingUser.setEmail(user.getEmail());
+        existingUser.setRole(user.getRole());
+        return userRepository.save(existingUser);
+    }
+    @DeleteMapping("/id/{id}")
+    public String deleteUserById(@PathVariable Long id)throws Exception{
+        Optional<User>otp=userRepository.findById(id);
+        if(otp.isEmpty()){
+            throw new Exception("User Not Found with id"+id);
+        }
+        userRepository.deleteById(otp.get().getId());
+        return "User Deleted";
     }
 }
--- backend/src/main/java/com/glowlink/marketplace/Model/User.java
+++ backend/src/main/java/com/glowlink/marketplace/Model/User.java
@@ -1,7 +1,10 @@
 package com.glowlink.marketplace.Model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
 
@@ -20,10 +23,21 @@ public class User {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @NotBlank(message = "UserName is Mandatory")
+    private String username;
+
     private String fullName;
+
+    @NotBlank(message = "Email is Mandatory")
     private String email;
+
     private String phone;
+    @NotBlank(message = "Role is Mandatory")
     private String role;
+    @NotBlank(message = "Password is Mandatory")
+    private String password;
+    @CreationTimestamp
     private LocalDateTime createdAt;
+    @UpdateTimestamp
     private LocalDateTime updatedAt;
 }
